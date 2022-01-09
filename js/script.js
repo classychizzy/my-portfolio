60bdf7a966610a27f927a6dea25848ee44cd7738
Chizoba Chukwuoma <45069751+classychizzy@users.noreply.github.com>
2022-01-09 14:31:19 +0100

    fixed error in footer and started work on form validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
-/* jquery is used for DOM manipulation */
+/* jquery is used for DOM manipulation and aos animation library is added */
 // eslint-disable-next-line no-undef
+AOS.init() // animate on scroll
 $(document).ready(function () {
- 
   $('.projects').css('margin-bottom', '50px')
   $('.target').css('margin-bottom', '50px')
 })
